4ba670878e457b387bcad029d3f61cd76b49dbde
JonBunator <62108893+JonBunator@users.noreply.github.com>
2025-04-16 11:37:04 +0200

    Add mui

---
+++ src/app/ui/Content.tsx
@@ -0,0 +1,8 @@
+"use client"
+import {Button} from "@mui/material";
+
+export default function Content() {
+    return (
+        <Button onClick={() => console.log("clicked")}>Test</Button>
+    );
+}
