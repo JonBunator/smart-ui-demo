ea2de7ad2a25ae87f931a80cb182ccdea1c43fd6
JonBunator <62108893+JonBunator@users.noreply.github.com>
2025-05-08 13:40:36 +0200

    Setup fluent-ui for nextjs

--- src/src/app/ui/Providers.tsx
+++ src/src/app/ui/Providers.tsx
@@ -1,14 +1,36 @@
 "use client"
 import {SmartComponentManager} from "smart-ui";
 import {SmartAgentProvider} from "smart-ui";
+import {
+    SSRProvider,
+    RendererProvider,
+    createDOMRenderer,
+    renderToStyleElements,
+} from '@fluentui/react-components';
+import { useServerInsertedHTML } from 'next/navigation';
 import {callAgentEndpoint} from "@/app/openAI/openAI";
-import {ReactNode} from "react";
+import {ReactNode, useRef, useState} from "react";
 
 export default function Providers({children}: {children: ReactNode}) {
+    const [renderer] = useState(() => createDOMRenderer());
+    const didRenderRef = useRef(false);
+
+    useServerInsertedHTML(() => {
+        if (didRenderRef.current) {
+            return;
+        }
+        didRenderRef.current = true;
+        return <>{renderToStyleElements(renderer)}</>;
+    });
+
     return (
         <SmartComponentManager>
             <SmartAgentProvider callAgent={callAgentEndpoint}>
-                {children}
+                <RendererProvider renderer={renderer}>
+                    <SSRProvider>
+                        {children}
+                    </SSRProvider>
+                </RendererProvider>
             </SmartAgentProvider>
         </SmartComponentManager>
     );
