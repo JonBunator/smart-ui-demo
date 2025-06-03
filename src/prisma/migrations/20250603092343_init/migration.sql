611fc831aeb9c708f8f25efdde90d87d6e4639e0
JonBunator <62108893+JonBunator@users.noreply.github.com>
2025-06-03 11:42:08 +0200

    Update database schema to concrete use cases

---
+++ src/prisma/migrations/20250603092343_init/migration.sql
@@ -0,0 +1,178 @@
+-- CreateEnum
+CREATE TYPE "DataType" AS ENUM ('Booking', 'Property', 'Maintenance');
+
+-- CreateEnum
+CREATE TYPE "DataCategory" AS ENUM ('Initial', 'GroundTruth', 'UserAdded');
+
+-- CreateEnum
+CREATE TYPE "SurveyState" AS ENUM ('NOT_STARTED', 'STARTED', 'USE_CASE_1', 'USE_CASE_1_QUESTIONS', 'USE_CASE_2', 'USE_CASE_2_QUESTIONS', 'USE_CASE_3', 'USE_CASE_3_QUESTIONS', 'FINISHED');
+
+-- CreateEnum
+CREATE TYPE "AISupport" AS ENUM ('NONE', 'AGENT', 'PROACTIVE_AGENT');
+
+-- CreateTable
+CREATE TABLE "Survey" (
+    "id" SERIAL NOT NULL,
+    "active" BOOLEAN NOT NULL,
+    "invitationCode" TEXT NOT NULL,
+    "nextAISupportOrder" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Survey_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Data" (
+    "id" SERIAL NOT NULL,
+    "order" INTEGER,
+    "category" "DataCategory" NOT NULL,
+    "type" "DataType" NOT NULL,
+
+    CONSTRAINT "Data_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" SERIAL NOT NULL,
+    "dataId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "eMail" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "houseNumber" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "bookingStart" TIMESTAMP(3) NOT NULL,
+    "bookingEnd" TIMESTAMP(3) NOT NULL,
+    "numAdults" INTEGER NOT NULL,
+    "numChildren" INTEGER NOT NULL,
+    "property" TEXT NOT NULL,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Property" (
+    "id" SERIAL NOT NULL,
+    "dataId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "houseNumber" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "numBedrooms" INTEGER NOT NULL,
+    "numBathrooms" INTEGER NOT NULL,
+    "numMaximumGuests" INTEGER NOT NULL,
+    "area" DOUBLE PRECISION NOT NULL,
+    "pricePerNight" DOUBLE PRECISION NOT NULL,
+    "deposit" DOUBLE PRECISION NOT NULL,
+    "additionalInfo" TEXT[],
+
+    CONSTRAINT "Property_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Maintenance" (
+    "id" SERIAL NOT NULL,
+    "dataId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "property" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "eMail" TEXT NOT NULL,
+    "urgency" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+
+    CONSTRAINT "Maintenance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "EMail" (
+    "id" SERIAL NOT NULL,
+    "subjectName" TEXT NOT NULL,
+    "subjectEmail" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "dataId" INTEGER NOT NULL,
+
+    CONSTRAINT "EMail_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Participation" (
+    "id" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "state" "SurveyState" NOT NULL DEFAULT 'NOT_STARTED',
+    "aiSupportOrder" INTEGER NOT NULL,
+    "promptHistory" TEXT,
+    "surveyId" INTEGER NOT NULL,
+
+    CONSTRAINT "Participation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UseCaseParticipation" (
+    "id" SERIAL NOT NULL,
+    "aiSupport" "AISupport" NOT NULL,
+    "participationId" TEXT NOT NULL,
+    "dataId" INTEGER NOT NULL,
+
+    CONSTRAINT "UseCaseParticipation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ParticipationData" (
+    "id" SERIAL NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "useCaseParticipationId" INTEGER NOT NULL,
+    "dataId" INTEGER NOT NULL,
+    "groundTruthId" INTEGER NOT NULL,
+
+    CONSTRAINT "ParticipationData_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Survey_invitationCode_key" ON "Survey"("invitationCode");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Booking_dataId_key" ON "Booking"("dataId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Property_dataId_key" ON "Property"("dataId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Maintenance_dataId_key" ON "Maintenance"("dataId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "EMail_dataId_key" ON "EMail"("dataId");
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_dataId_fkey" FOREIGN KEY ("dataId") REFERENCES "Data"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Property" ADD CONSTRAINT "Property_dataId_fkey" FOREIGN KEY ("dataId") REFERENCES "Data"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Maintenance" ADD CONSTRAINT "Maintenance_dataId_fkey" FOREIGN KEY ("dataId") REFERENCES "Data"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EMail" ADD CONSTRAINT "EMail_dataId_fkey" FOREIGN KEY ("dataId") REFERENCES "Data"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Participation" ADD CONSTRAINT "Participation_surveyId_fkey" FOREIGN KEY ("surveyId") REFERENCES "Survey"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UseCaseParticipation" ADD CONSTRAINT "UseCaseParticipation_participationId_fkey" FOREIGN KEY ("participationId") REFERENCES "Participation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ParticipationData" ADD CONSTRAINT "ParticipationData_useCaseParticipationId_fkey" FOREIGN KEY ("useCaseParticipationId") REFERENCES "UseCaseParticipation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ParticipationData" ADD CONSTRAINT "ParticipationData_dataId_fkey" FOREIGN KEY ("dataId") REFERENCES "Data"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ParticipationData" ADD CONSTRAINT "ParticipationData_groundTruthId_fkey" FOREIGN KEY ("groundTruthId") REFERENCES "Data"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,4 +1,4 @@
-import {Prisma, PrismaClient } from "../generated/prisma";
+import {DataCategory, DataType, Prisma, PrismaClient} from "../generated/prisma";
 
 const prisma = new PrismaClient();
 
@@ -6,28 +6,48 @@ const surveyData: Prisma.SurveyCreateInput[] = [
     {
         active: true,
         invitationCode: "invite",
-        UseCases: {
-            create: [
-                {
-                    url: "bookings",
-                    order: 0
-                },
-                {
-                    url: "properties",
-                    order: 1,
-                },
-                {
-                    url: "maintenance",
-                    order: 2
-                },
-            ]
-        }
     }
 ];
+
+const mainData:  Prisma.DataCreateInput[] = [
+    {
+        category: DataCategory.GroundTruth,
+        type: DataType.Booking,
+        order: 0,
+        EMail: {
+            create: {
+                subjectName: "Luke Skywalker",
+                subjectEmail: "luke@rebellion.com",
+                content: "Hi I am Luke"
+            }
+        },
+        Booking: {
+            create: {
+                name: "Luke",
+                surname: "Skywalker",
+                phoneNumber: "1234567890",
+                eMail: "luke@rebellion.com",
+                street: "Some street 1",
+                houseNumber: "4",
+                city: "City 1",
+                postalCode: "12345",
+                country: "Germany",
+                bookingStart: new Date(),
+                bookingEnd: new Date(),
+                numAdults: 2,
+                numChildren: 0,
+                property: "Death Star",
+            }
+        }
+    }
+]
 
 export async function main() {
     for (const sd of surveyData) {
         await prisma.survey.create({ data: sd });
+    }
+    for (const md of mainData) {
+        await prisma.data.create({ data: md });
     }
 }
 
