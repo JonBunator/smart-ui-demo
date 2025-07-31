998001b5afbce91beeb1510d8b03115021ac672a
JonBunator <62108893+JonBunator@users.noreply.github.com>
2025-07-31 16:37:05 +0200

    Add page descriptions

--- src/src/app/ui/providers/Providers.tsx
+++ src/src/app/ui/providers/Providers.tsx
@@ -7,14 +7,48 @@ import FluentUIProvider from "./FluentUIProvider";
 import MUIProvider from "@/app/ui/providers/MUIProvider";
 import SurveyManagerProvider from "@/app/ui/propertyManagement/surveyManager/SurveyManagerProvider";
 import {CUSTOM_SYSTEM_PROMPT} from "@/lib/config";
+import { usePathname } from "next/navigation";
+
+const pageDescriptions = [
+    {
+        path: "/survey/bookings",
+        description: "Is used to manage bookings and add new ones. Displays bookings in table",
+    },
+    {
+        path: "/survey/bookings/add",
+        description: "Form that is used to add a new booking",
+    },
+    {
+        path: "/survey/properties",
+        description: "Is used to manage vacation homes and add new ones. Displays properties in cards with images of them",
+    },
+    {
+        path: "/survey/properties/add",
+        description: "Form that is used to add a new vacation home",
+    },
+    {
+        path: "/survey/maintenance",
+        description: "Is used to manage maintenances of vacation homes and add new ones. Displays maintenances in table",
+    },
+    {
+        path: "/survey/maintenance/add",
+        description: "Form that is used to add a new maintenance request",
+    }
+]
 
 export default function Providers({children}: {children: ReactNode}) {
+
+    const currentPagePath = usePathname();
+
     return (
         <FluentUIProvider>
             <MUIProvider>
                 <SurveyManagerProvider>
                     <SmartComponentManager>
-                        <SmartAgentProvider callAgent={callAgentEndpoint} customSystemPrompt={CUSTOM_SYSTEM_PROMPT}>
+                        <SmartAgentProvider callAgent={callAgentEndpoint}
+                                            customSystemPrompt={CUSTOM_SYSTEM_PROMPT}
+                                            currentPagePath={currentPagePath}
+                                            pageDescriptions={pageDescriptions}>
                             {children}
                         </SmartAgentProvider>
                     </SmartComponentManager>
